aafea0fadbdd5fa4c9b932d2094550dcfcd2d9c0
bannonco <bannonco@tcd.ie>
2017-03-04 17:30:45 +0000

    removed irrelevant code and updated the rest
    Please enter the commit message for your changes. Lines starting

---
+++ commands.py
@@ -0,0 +1,20 @@
+
+
+
+class Commands:
+
+	def __init__(self,hostname,portnum):
+		self.hostname=hostname
+		self.portnum=portnum
+		
+	def changedirectory(self,filename,connect):	
+		if filename == "..":
+		
+		else:
+		
+	def list(self,,connect):
+	
+	def read(self,,connect):
+	
+	def write(self,connect):
+		
--- server.py
+++ server.py
@@ -2,11 +2,10 @@ import sys,os
 from socket import *
 from Queue import Queue
 from threading import Thread
-import collections
-from chat_room import ChatRoom
+from commands import Commands
 
-chat_room=ChatRoom(sys.argv[1],int(sys.argv[2]))
-rooms=collections.OrderedDict()
+
+
 threads=[]
 activE=True
 threadLimit=100
@@ -23,56 +22,14 @@ sock.listen(10)
 def messages(connect):
 	while activE:
 		msg=connect.recv(2048)
-		if msg[:13] == "JOIN_CHATROOM":
-			print("joining chatroom")
-			msgs=msg.split("\n")
-			chatroom=msgs[0].split(":")[1]
-			client_name=msgs[3].split(":")[1]
-			numb=0
-			for c in chatroom:
-				numb=numb+ord(c)
-			room_ref=numb%10
-			number=0
-			for c in client_name:
-				number=number+ord(c)
-			join_id=number%10
-			chat_room.joinroom(rooms,chatroom,client_name,room_ref,join_id,connect)
-			#retmsg="JOINED_CHATROOM:%s\nSERVER_IP:%s\nPORT:%s\nROOM_REF:%s\nJOIN_ID:%s\n"%(chatroom,sys.argv[1],int(sys.argv[2]),room_ref,join_id)
-			#connect.sendall(retmsg)
-			print("joined chatroom")
-		elif msg[:14] =="LEAVE_CHATROOM":
-			print("leaving chatroom")
-			msgs=msg.split("\n")
-			room_ref=int(msgs[0].split(":")[1])
-			join_id=int(msgs[1].split(":")[1])
-			client_name=msgs[2].split(":")[1]
-			chat_room.leaveroom(rooms,room_ref,join_id,client_name,connect)
-			#retmsg="LEFT_CHATROOM:%s\nJOIN_ID:%s\n"%(str(room_ref),str(join_id))
-			#connect.sendall(retmsg)
-			print("left chatroom")
-		elif msg[:10] =="DISCONNECT":
-			print("client disconnecting")
-			msgs=msg.split("\n")
-			client_name=msgs[2].split(":")[1]
-			number=0
-			for c in client_name:
-				number=number+ord(c)
-			join_id=number%10
-			chat_room.disconnect(rooms,join_id,client_name,connect,activE)
-			#retmsg="DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: Name\n"
-			#connect.sendall(retmsg)
-			print("client disconnected")	
-		elif msg[:4] =="CHAT":
-			print("received message")
-			msgs=msg.split("\n")
-			room_ref=int(msgs[0].split(":")[1])
-			join_id=int(msgs[1].split(":")[1])
-			client_name=msgs[2].split(":")[1]
-			data=msgs[3].split(":")[1]
-			chat_room.chat(rooms,room_ref,join_id,client_name,data,connect)	
-			#retmsg="CHAT:%s\nCLIENT_NAME:%s\nMESSAGE:%s\n\n"%(str(room_ref),str(client_name),data)
-			#connect.sendall(retmsg)
-			print("message received")
+		if msg[:2] == "cd":
+		
+		elif msg=="ls":
+			
+		elif msg[:4] =="read":
+				
+		elif msg[:5] =="write":
+			
 		elif msg[:4]=="HELO":
 				print "HELO Received: "+msg
 				msg="%sIP:%s\nPort:%s\nStudentID:13319829\n"%(msg,str(gethostbyname(gethostname())),int(sys.argv[2]))
@@ -80,6 +37,7 @@ def messages(connect):
 				print "HELO Sent"
 		elif msg[:12]=="KILL_SERVICE":				
 				os._exit(1)
+				activE=False
 		else:
 			errmsg="ERROR_CODE:1\nERROR_DESCRIPTION: Parse error\n"
 			connect.sendall(errmsg)
@@ -95,5 +53,4 @@ while activE:
 		numb_of_threads=numb_of_threads+1
 	else:
 		print("max threads reached")
-#if __name__ == "__main__":
-#	chat_server(sys.argv[1],int(sys.argv[2]),int(sys.argv[3]))
+
